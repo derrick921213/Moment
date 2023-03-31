92dff08f3d3fe997869281b22c870e6ea9a911fc
peter <peterchen2017125@gmail.com>
2023-03-31 15:16:32 +0800

    20230331

--- Front/src/index.js
+++ Front/src/index.js
@@ -1,2 +1,15 @@
 import "./index.scss";
 import Collapse from "bootstrap/js/dist/collapse";
+Vue.createApp({
+  data() {
+    return {
+      products: []
+    }
+  },
+  created() {
+    let url = "https://fakestoreapi.com/products";
+    fetch(url).then(res => res.json()).then(data => {
+      this.products = data;
+    })
+  }
+}).mount("#app");
