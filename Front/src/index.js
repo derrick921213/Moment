e87f5ed5f4d8d513e81a9a709a9b24cde4db880d
peter <peterchen2017125@gmail.com>
2023-04-10 19:54:06 +0800

    20230410

--- Front/src/index.js
+++ Front/src/index.js
@@ -8,8 +8,19 @@ Vue.createApp({
   },
   created() {
     let url = "https://fakestoreapi.com/products";
+    // let url = "/Back/PHP/readJSON/index.php"
     fetch(url).then(res => res.json()).then(data => {
       this.products = data;
     })
+  },
+  methods: {
+    minus(product) {
+      product.amountShow--;
+      product.amountShow = (product.amountShow < 1) ? 0 : product.amountShow
+    },
+    plus(product) {
+      product.amountShow++;
+      product.amountShow = (product.amountShow > 9) ? 9 : product.amountShow
+    },
   }
 }).mount("#app");
