bb8a6f972c7b9935647e47f58028e663793a0e94
peter <peterchen2017125@gmail.com>
2023-04-26 23:17:47 +0800

    20230426

--- Front/src/index.js
+++ Front/src/index.js
@@ -1,43 +1,3 @@
-// import "./index.scss";
-// import Modal from "bootstrap/js/dist/modal";
-// Vue.createApp({
-//   data() {
-//     return {
-//       products: [],
-//       amountShow: 0,
-//     }
-//   },
-//   created() {
-//     let url = "https://moment.duacodie.com/Back/PHP/readJSON/product.json"
-//     fetch(url).then(res => res.json()).then(data => {
-//       this.products = data;
-//     })
-//   },
-//   methods: {
-//     minus(product) {
-//       product.amountShow--;
-//       product.amountShow = (product.amountShow < 1) ? 0 : product.amountShow
-//     },
-//     plus(product) {
-//       product.amountShow++;
-//       product.amountShow = (product.amountShow > 9) ? 9 : product.amountShow
-//     },
-//   }
-// }).mount("#app");
-
-// function modalHandler() {
-//   const cartBtn = document.getElementById("cart-btn");
-//   const cart = new Modal(document.getElementById("cart"), {
-//       keyboard: false,
-//   });
-
-//   cartBtn.addEventListener("click", function () {
-//       cart.toggle();
-//   });
-// }
-// modalHandler();
-
-
 import "./index.scss";
 import Modal from "bootstrap/js/dist/modal";
 Vue.createApp({
@@ -45,47 +5,70 @@ Vue.createApp({
     return {
       products: [],
       cartItems: [],
-    }
+      amountShow: 0,
+    };
   },
+  methods: {
+    minus(product) {
+      this.addToCart(product, false);
+    },
+    plus(product) {
+      this.addToCart(product);
+    },
+    addToCart(product, increase = true) {
+      const index = this.cartItems.findIndex((item) => item.id === product.id);
+      if (index > -1) {
+        this.cartItems[index].amount += increase ? 1 : -1;
+        this.cartItems[index].amount = this.cartItems[index].amount < 1 ? 0 : this.cartItems[index].amount > 9 ? 9 : this.cartItems[index].amount;
+        if (this.cartItems[index].amount === 0) {
+          this.removeFromCart(index);
+        }
+      } else {
+        this.cartItems.push({
+          id: product.id,
+          title: product.title,
+          price: product.price,
+          amount: increase ? 1 : 0,
+        });
+      }
+      const cartItem = this.cartItems.find((item) => item.id === product.id);
+      product.amountShow = cartItem ? cartItem.amount : 0;
+    },
+    removeFromCart(index) {
+      const productId = this.cartItems[index].id;
+      this.cartItems.splice(index, 1);
+      const product = this.products.find((product) => product.id === productId);
+      if (product) {
+        product.amountShow = 0;
+      }
+    },
+    getTotalPrice() {
+      return this.cartItems.reduce((total, item) => {
+        return total + item.price * item.amount;
+      }, 0);
+    },
+  },
+  created() {
+    let url = "https://moment.duacodie.com/Back/PHP/readJSON/product.json";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        this.products = data;
+        this.products.forEach((product) => {
+          product.amountShow = 0;
+        });
+      });
+  },
+
   created() {
     let url = "https://moment.duacodie.com/Back/PHP/readJSON/product.json"
     fetch(url).then(res => res.json()).then(data => {
       this.products = data;
+      this.products.forEach((product) => {
+        product.amountShow = 0;
+      });
     })
   },
-  methods: {
-    addToCart(product) {
-      let index = this.cartItems.findIndex(item => item.id === product.id);
-      if (index === -1) {
-        this.cartItems.push({ id: product.id, title: product.title, price: product.price, quantity: 1 });
-      } else {
-        this.cartItems[index].quantity++;
-      }
-    },
-    removeFromCart(item) {
-      let index = this.cartItems.findIndex(cartItem => cartItem.id === item.id);
-      this.cartItems.splice(index, 1);
-    },
-    getTotalPrice() {
-      return this.cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
-    },
-    getCartCount() {
-      return this.cartItems.reduce((total, item) => total + item.quantity, 0);
-    },
-    increaseQuantity(item) {
-      item.quantity++;
-    },
-    decreaseQuantity(item) {
-      if (item.quantity > 1) {
-        item.quantity--;
-      }
-    }
-  },
-  computed: {
-    cartIsEmpty() {
-      return this.cartItems.length === 0;
-    }
-  }
 }).mount("#app");
 
 function modalHandler() {
@@ -99,3 +82,67 @@ function modalHandler() {
   });
 }
 modalHandler();
+
+
+// import "./index.scss";
+// import Modal from "bootstrap/js/dist/modal";
+// Vue.createApp({
+//   data() {
+//     return {
+//       products: [],
+//       cartItems: [],
+//     }
+//   },
+//   created() {
+//     let url = "https://moment.duacodie.com/Back/PHP/readJSON/product.json"
+//     fetch(url).then(res => res.json()).then(data => {
+//       this.products = data;
+//     })
+//   },
+//   methods: {
+//     addToCart(product) {
+//       let index = this.cartItems.findIndex(item => item.id === product.id);
+//       if (index === -1) {
+//         this.cartItems.push({ id: product.id, title: product.title, price: product.price, quantity: 1 });
+//       } else {
+//         this.cartItems[index].quantity++;
+//       }
+//     },
+//     removeFromCart(item) {
+//       let index = this.cartItems.findIndex(cartItem => cartItem.id === item.id);
+//       this.cartItems.splice(index, 1);
+//     },
+//     getTotalPrice() {
+//       return this.cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+//     },
+//     getCartCount() {
+//       return this.cartItems.reduce((total, item) => total + item.quantity, 0);
+//     },
+//     increaseQuantity(item) {
+//       item.quantity++;
+//     },
+//     decreaseQuantity(item) {
+//       if (item.quantity > 1) {
+//         item.quantity--;
+//       }
+//     }
+//   },
+//   computed: {
+//     cartIsEmpty() {
+//       return this.cartItems.length === 0;
+//     }
+//   }
+// }).mount("#app");
+
+// function modalHandler() {
+//   const cartBtn = document.getElementById("cart-btn");
+//   const cart = new Modal(document.getElementById("cart"), {
+//     keyboard: false,
+//   });
+
+//   cartBtn.addEventListener("click", function () {
+//     cart.toggle();
+//   });
+// }
+// modalHandler();
+
