1c97bf487bd19da0858bd87a06a1c05c172ddc8a
derrick921213 <dlin12457@gmail.com>
2023-05-03 16:58:47 +0800

    fix

--- Back/PHP/dashTwig-fix/core/controller.Class.php
+++ Back/PHP/dashTwig-fix/core/controller.Class.php
@@ -5,8 +5,8 @@ session_start();
 class Connect extends PDO{
     public function __construct($user,$passwd){
         parent::__construct("mysql:host=localhost;dbname=Moment",$user,$passwd,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
-        // $this->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        // $this->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+        $this->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $this->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
     }
 }
 class Controller{
