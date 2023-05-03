90026baa09a449c764718c2f9f71629dbc473717
derrick921213 <dlin12457@gmail.com>
2023-05-03 13:31:35 +0800

    fix

--- Back/PHP/dashTwig-fix/index.php
+++ Back/PHP/dashTwig-fix/index.php
@@ -5,6 +5,9 @@ require_once(Core_PATH.'controller.Class.php');
 session_start();
 echo $_SERVER['PHP_SELF'];
 $q = explode('/',$_SERVER['PHP_SELF']);
+array_pop($q);
+print_r($q);
+$q = implode("/", $q);
 print_r($q);
 ?>
 <!DOCTYPE html>
