78bccbb723e89454d0c1592d211209a8315e06c4
peter <peterchen2017125@gmail.com>
2023-06-19 20:30:10 +0800

    test update

--- Front/src/index.js
+++ Front/src/index.js
@@ -77,8 +77,8 @@ Vue.createApp({
 
   },
   created() {
-    // let url = "https://moment.duacodie.com/Back/PHP/readJSON/product.json"
-    let url = "https://moment.duacodie.com/api/menu";
+    let url = "https://fakestoreapi.com/products"
+    // let url = "https://moment.duacodie.com/api/menu";
     fetch(url).then(res => res.json()).then(data => {
       this.products = data;
       this.products.forEach((product) => {
